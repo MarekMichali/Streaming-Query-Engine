fa4fd962b0abc42e3a6d8df29a451c6130246846
tomek <tomakry729@student.polsl.pl>
2022-06-05 11:22:27 +0200

    Added custom exceptions for invalid parameters and exception handlers in controller

--- consumer/src/main/java/pl/polsl/hdised/consumer/query/QueryController.java
+++ consumer/src/main/java/pl/polsl/hdised/consumer/query/QueryController.java
@@ -1,12 +1,11 @@
 package pl.polsl.hdised.consumer.query;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import pl.polsl.hdised.consumer.averageresponse.AverageResponseDto;
+import pl.polsl.hdised.consumer.exception.ParametersNotFoundException;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 @RequestMapping("api/v1/query")
 @RestController
@@ -19,19 +18,13 @@ public class QueryController {
     }
 
     @GetMapping
-    public AverageResponseDto getHistoricalAverage(@RequestParam("deviceId") String deviceId, @RequestParam("location") String location, @RequestParam("stringDate") String stringDate) throws ParseException {
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
-        Date date = format.parse(stringDate);
-
-        return this.queryService.getHistoricalAverage(deviceId, location, date);
+    public AverageResponseDto getHistoricalAverage(@RequestParam("deviceId") String deviceId, @RequestParam("location") String location, @RequestParam("stringDate") String stringDate) throws ParseException, ParametersNotFoundException {
+        return this.queryService.getHistoricalAverage(deviceId, location, stringDate);
     }
 
     @PostMapping("/set-query-parameters")
-    public String setQueryParameters(@RequestParam("deviceId") String deviceId, @RequestParam("location") String location, @RequestParam("stringDate") String stringDate) throws ParseException {
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
-        Date date = format.parse(stringDate);
-
-        this.queryService.setQueryParameters(deviceId, location, date);
+    public String setQueryParameters(@RequestParam("deviceId") String deviceId, @RequestParam("location") String location, @RequestParam("stringDate") String stringDate) throws ParseException, ParametersNotFoundException {
+        this.queryService.setQueryParameters(deviceId, location, stringDate);
 
         return "Parameters set successfully";
     }
@@ -39,5 +32,23 @@ public class QueryController {
     @GetMapping("/get-average")
     public Float getStreamAverage() {
         return this.queryService.getStreamAverage();
+    }
+
+    @ExceptionHandler(ParseException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    String handleException(ParseException parseException) {
+        return "Provided date format is invalid";
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    String handleException(NullPointerException nullPointerException) {
+        return nullPointerException.getMessage();
+    }
+
+    @ExceptionHandler(ParametersNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    String handleException(ParametersNotFoundException parametersNotFoundException) {
+        return parametersNotFoundException.getMessage();
     }
 }
--- consumer/src/main/java/pl/polsl/hdised/consumer/query/QueryService.java
+++ consumer/src/main/java/pl/polsl/hdised/consumer/query/QueryService.java
@@ -3,46 +3,70 @@ package pl.polsl.hdised.consumer.query;
 
 import org.springframework.stereotype.Service;
 import pl.polsl.hdised.consumer.averageresponse.AverageResponseDto;
+import pl.polsl.hdised.consumer.date.DateRepository;
+import pl.polsl.hdised.consumer.device.DeviceRepository;
+import pl.polsl.hdised.consumer.exception.ParametersNotFoundException;
+import pl.polsl.hdised.consumer.location.LocationRepository;
 import pl.polsl.hdised.consumer.measurement.MeasurementRepository;
 
 import javax.persistence.Tuple;
 import java.math.BigInteger;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Objects;
 
 @Service
 public class QueryService {
 
     private MeasurementRepository measurementRepository;
+    private DeviceRepository deviceRepository;
+    private LocationRepository locationRepository;
+    private DateRepository dateRepository;
 
-    public QueryService(MeasurementRepository measurementRepository) {
+    public QueryService(MeasurementRepository measurementRepository, DeviceRepository deviceRepository, LocationRepository locationRepository, DateRepository dateRepository) {
         this.measurementRepository = measurementRepository;
+        this.deviceRepository = deviceRepository;
+        this.locationRepository = locationRepository;
+        this.dateRepository = dateRepository;
     }
 
-    public AverageResponseDto getHistoricalAverage(String deviceId, String location, Date date){
-        validateParams(deviceId, location, date);
-        Tuple tuple = this.measurementRepository.getAverage(deviceId, location, date);
+    public AverageResponseDto getHistoricalAverage(String deviceId, String location, String stringDate) throws ParseException, ParametersNotFoundException {
+        if (!parametersExists(deviceId, location)) {
+            throw new ParametersNotFoundException();
+        }
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+        Date date = format.parse(stringDate);
+
+        Tuple tuple = this.measurementRepository.getAverage(Objects.requireNonNull(deviceId, "device id cannot be null"), Objects.requireNonNull(location, "location cannot be null"), Objects.requireNonNull(date, "date cannot be null"));
+
         Double avgTemp = (Double) tuple.get("averageTemperature");
         BigInteger tempsCount = (BigInteger) tuple.get("temperaturesCount");
         return new AverageResponseDto(avgTemp.floatValue(), tempsCount.intValue());
     }
 
-    public void setQueryParameters(String deviceId, String location, Date date){
-        validateParams(deviceId, location, date);
+    public void setQueryParameters(String deviceId, String location, String stringDate) throws ParseException, ParametersNotFoundException {
+        if (!parametersExists(deviceId, location)) {
+            throw new ParametersNotFoundException();
+        }
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+        Date date = format.parse(stringDate);
+
         Query query = Query.getInstance();
-        query.setParameters(location, deviceId, date);
+        query.setParameters(Objects.requireNonNull(location, "location cannot be null"), Objects.requireNonNull(deviceId, "device id cannot be null"), Objects.requireNonNull(date, "date cannot be null"));
     }
 
-    public Float getStreamAverage(){
+    public Float getStreamAverage() {
         Query query = Query.getInstance();
 
         return query.getTemperaturesSum() / query.getTemperaturesCount();
     }
 
-    private void validateParams(String deviceId, String location, Date date){
-        Objects.requireNonNull(deviceId, "device id cannot be null");
-        Objects.requireNonNull(location, "location cannot be null");
-        Objects.requireNonNull(date, "date id cannot be null");
+    private Boolean parametersExists(String deviceId, String location) {
+        return this.deviceRepository.findDeviceById(deviceId) != null && this.locationRepository.findLocationByCity(location) != null;
     }
 
 }
